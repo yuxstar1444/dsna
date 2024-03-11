b393ece8d2466c7ac0625baad10c59aea3ce7869
yuxstar1444@protonmail.com <yuxstar1444@protonmail.com>
2024-03-11 15:08:40 -0400

    two sum ii python

---
+++ python/162 Two Sum II.py
@@ -0,0 +1,16 @@
+#Brute force, start with index 0, iterate through entire array, O(n^2)
+# 2 Pointer: array is sorted, we can use this advantage
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l ,r = 0, len(numbers) - 1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            if curSum > target:
+                r -= 1
+            elif curSum < target: 
+                l+= 1
+            else: 
+                return [l+1, r+1]
+
+        
